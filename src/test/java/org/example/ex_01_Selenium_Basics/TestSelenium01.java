d45954ae6aea568899a6208043ba8bd36f1e9b5f
pavani <muthyalapavanireddy1997@gmail.com>
2025-06-16 23:30:23 +0530

    Setup

---
+++ src/test/java/org/example/ex_01_Selenium_Basics/TestSelenium01.java
@@ -0,0 +1,19 @@
+package org.example.ex_01_Selenium_Basics;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium01 {
+
+    @Test
+    public void test_VerifyVWOLogin(){
+        // Write the code which will execute the perform the ui interactions
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://app.vwo.com");
+        System.out.println(driver.getSessionId());
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+
+}
