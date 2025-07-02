55c32e88171dded63413b249357e208ac2e56b47
pavani <muthyalapavanireddy1997@gmail.com>
2025-07-02 23:13:19 +0530

    2 july

--- src/test/java/org/example/ex_04_Selenium_Waits/TestSelenium26_FluentWait.java
+++ src/test/java/org/example/ex_04_Selenium_Waits/TestSelenium26_FluentWait.java
@@ -1,4 +1,90 @@
 package org.example.ex_04_Selenium_Waits;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.function.Function;
+
 public class TestSelenium26_FluentWait {
-}
+
+
+    @Test(description = "Verify that with invalid email, pass, error message is shown on the app.vwo.com")
+    public void test_negative_vwo_login() throws Exception {
+
+        EdgeOptions edgeOptions = new EdgeOptions();
+        // edgeOptions.addArguments("--start-maximized");
+
+        WebDriver driver = new EdgeDriver(edgeOptions);
+//        driver.navigate().to("https://app.vwo.com");
+        driver.get("https://app.vwo.com");
+
+        driver.manage().window().maximize();
+
+
+        //  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement emailInputBox = driver.findElement(By.id("login-username"));
+        emailInputBox.sendKeys("admin@admin.com");
+
+
+        WebElement passwordInputBox = driver.findElement(By.name("password"));
+        passwordInputBox.sendKeys("admin");
+
+
+        WebElement buttonSubmit = driver.findElement(By.id("js-login-btn"));
+        buttonSubmit.click();
+
+// Jvm hold
+//            try {
+//                Thread.sleep(3000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+
+
+//        Explicit wait
+//        WebDriverWait wait  = new WebDriverWait(driver, Duration.ofSeconds(5));
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-cy='closeModal']")));
+
+        //Fluent wait
+
+        FluentWait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(2)).ignoring(NoSuchElementException.class);
+
+        WebElement error_message = wait.until(new Function<WebDriver, WebElement>() {
+            @Override
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.className("notification-box-description"));
+            }
+        });
+
+
+        //until this  notification displays or visible we have to wait for max =3 sec
+
+
+        //  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("notification-box-description")));
+        // WebElement error_message= driver.findElement(By.className("notification-box-description"));
+
+
+        //one is enough
+        //   wait.until(ExpectedConditions.textToBePresentInElement(error_message,"Your email, password, IP address or location did not match"));
+
+
+        Assert.assertEquals(error_message.getText(),"Your email, password, IP address or location did not match");
+
+
+        driver.quit();
+
+
+    }}
---
+++ src/test/java/org/example/ex_05_Selenium_Input_Select_Alerts/TestSelenium27_.java
@@ -0,0 +1,6 @@
+package org.example.ex_05_Selenium_Input_Select_Alerts;
+
+public class TestSelenium27_
+
+{
+}
